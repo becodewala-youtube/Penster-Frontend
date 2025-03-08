ecb8aa92edd538220dd12505dd87406cd4c26a3c
becodewala-youtube <kvikash74673@gmail.com>
2025-03-08 10:42:04 +0530

    skeleton to post details

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { ThumbsUp, Share2, Bookmark, Clock, Edit, Trash2 } from 'lucide-react';
 import ReactMarkdown from 'react-markdown';
 import { useAuth } from '../context/AuthContext';
+import Skeleton from '../components/Skeleton';
 
 interface Comment {
   _id: string;
@@ -221,9 +222,7 @@ const PostDetail = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-      </div>
+      <Skeleton/>
     );
   }
 
