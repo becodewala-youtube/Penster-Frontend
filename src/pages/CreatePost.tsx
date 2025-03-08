fa42704dbb743e6db8fd9023631abb82ddbdfb9e
becodewala-youtube <kvikash74673@gmail.com>
2025-03-08 10:45:31 +0530

    create post responsive for small devices

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -149,7 +149,7 @@ const CreatePost = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto md:p-6">
       <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6">
         <div className="flex items-center justify-between mb-6">
           <div className="flex items-center space-x-2">
